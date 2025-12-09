298a88a895f85d00a0eda246cf81825a62b4c424
Reza Darzi <darzi.reza@gmail.com>
2025-12-09 21:42:39 +0330

    feat: Add __all__ in __init__.py

---
+++ transformers/vanilla/__init__.py
@@ -0,0 +1,8 @@
+# __init__
+#
+
+from vanilla.positional_encoding import PositionalEncoding
+from vanilla.attention import scaled_dot_product_attention
+
+
+__all__ = ["PositionalEncoding", "scaled_dot_product_attention"]
