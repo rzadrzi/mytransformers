f5cd695de7e48dc57fb0fc4159fdb57d86e6f90d
Reza Darzi <darzi.reza@gmail.com>
2025-12-11 00:57:33 +0330

    feat: Add classes in __init__

--- transformers/vanilla/__init__.py
+++ transformers/vanilla/__init__.py
@@ -4,9 +4,13 @@
 from vanilla.positional_encoding import PositionalEncoding
 from vanilla.multi_head_attention import MultiHeadAttention
 from vanilla.position_wise_feed_forward_network import PositionWiseFeedForward
+from vanilla.encoder import Encoder
+from vanilla.decoder import Decoder
 
 __all__ = [
     "PositionalEncoding",
     "MultiHeadAttention",
     "PositionWiseFeedForward",
+    "Encoder",
+    "Decoder",
 ]
