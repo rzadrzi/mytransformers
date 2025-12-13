59ea8824a078ff368f182a09222177eb84841055
Reza Darzi <darzi.reza@gmail.com>
2025-12-14 02:44:29 +0330

    feat: Change positional encoding from function to a Flax class

--- transformers/vanilla_JAX/positional_encoding.py
+++ transformers/vanilla_JAX/positional_encoding.py
@@ -3,8 +3,30 @@
 
 import jax
 import jax.numpy as jnp
+from flax import nnx
 
 
+class PositionalEncoding(nnx.Module):
+    def setup(self, d_model: int, max_len: int, base=1000.0, dtype=jnp.float32) -> None:
+        assert d_model % 2 == 0, "d_model must be even in PositionalEncoding"
+        pos = jnp.arange(max_len, dtype=dtype)[:, None]
+        i = jnp.arange(0, d_model, 2, dtype=dtype)[None, :]
+
+        inv_freq = 1.0 / (base ** (i / d_model))
+
+        angle = pos * inv_freq
+
+        pe = jnp.zeros((max_len, d_model), dtype=dtype)
+        pe = pe.at[:, 0::2].set(jnp.sin(angle))
+        pe = pe.at[:, 1::2].set(jnp.cos(angle))
+        self.pe = jax.device_put(pe)
+
+    def __call__(self, x):
+        x = x + self.pe[:, : x.shape[1]]
+        return x
+
+
+"""
 def positional_encoding(
     d_model: int, max_len: int, base=1000.0, dtype=jnp.float32
 ) -> jnp.ndarray:
@@ -21,3 +43,5 @@ def positional_encoding(
     pe = pe.at[:, 0::2].set(jnp.sin(angle))
     pe = pe.at[:, 1::2].set(jnp.cos(angle))
     return pe
+
+"""
